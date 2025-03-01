a32bf6ba35cba9369f470c0f5bd83dfbc5069895
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-01 11:02:34 +0200

    Add endpoint to retrieve a contact by ID

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import { getEnvVar } from './utils/getEnvVar.js';
-import { getAllContacts } from './services/contacts.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 const PORT = getEnvVar('PORT', 3000);
 
@@ -25,6 +25,22 @@ export function setupServer() {
       status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      res.status(404).json({ message: 'Contact not found' });
+      return;
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
     });
   });
 
