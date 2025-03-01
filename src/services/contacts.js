c91536f20faae67a82a42c19b841bbe4435253da
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-01 10:27:34 +0200

    Add service to retrieve a contact by ID

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -4,3 +4,8 @@ export async function getAllContacts() {
   const contacts = await ContactsCollection.find();
   return contacts;
 }
+
+export async function getContactById(contactId) {
+  const contact = await ContactsCollection.findById(contactId);
+  return contact;
+}
