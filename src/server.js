2fea73237119c2be7bf9b9f76ed75c368b78120e
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-01 10:27:13 +0200

    Add endpoint to retrieve all contacts

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts } from './services/contacts.js';
 
 const PORT = getEnvVar('PORT', 3000);
 
@@ -17,6 +18,15 @@ export function setupServer() {
       },
     }),
   );
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
 
   app.use((req, res) => {
     res.status(404).json({
