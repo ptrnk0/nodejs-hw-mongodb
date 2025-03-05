1842cdd79fe0942519cc3b5a0492340a84d32986
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-05 13:44:29 +0200

    Add PATCH and DELETE routes for updating and removing contacts

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import {
   createContactController,
+  deleteContactController,
   getAllContactsController,
   getContactController,
+  updateContactContoller,
 } from '../controllers/contacts.js';
 import ctrlWrapper from '../utils/ctrlWrapper.js';
 
@@ -11,5 +13,13 @@ const contactsRouter = Router();
 contactsRouter.get('/contacts', ctrlWrapper(getAllContactsController));
 contactsRouter.get('/contacts/:contactId', ctrlWrapper(getContactController));
 contactsRouter.post('/contacts', ctrlWrapper(createContactController));
+contactsRouter.patch(
+  '/contacts/:contactId',
+  ctrlWrapper(updateContactContoller),
+);
+contactsRouter.delete(
+  '/contacts/:contactId',
+  ctrlWrapper(deleteContactController),
+);
 
 export default contactsRouter;
