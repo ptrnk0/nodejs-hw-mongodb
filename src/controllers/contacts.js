e285225d86319c6195e31848b5bd601139ffc511
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-05 11:54:08 +0200

    Add controllers for retrieving contacts

---
+++ src/controllers/contacts.js
@@ -0,0 +1,26 @@
+import { getAllContacts, getContactById } from '../services/contacts.js';
+
+export const getAllContactsController = async (req, res) => {
+  const contacts = await getAllContacts();
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
+};
+
+export const getContactController = async (req, res) => {
+  const { contactId } = req.params;
+  const contact = await getContactById(contactId);
+
+  if (!contact) {
+    res.status(404).json({ message: 'Contact not found' });
+    return;
+  }
+
+  res.status(200).json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
+};
