55b5e60a6d4a51d243faaa4dcefd80b5e713c7aa
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-05 12:28:50 +0200

    Refactor getContactController to use http-errors for improved error handling

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,3 +1,4 @@
+import createHttpError from 'http-errors';
 import { getAllContacts, getContactById } from '../services/contacts.js';
 
 export async function getAllContactsController(req, res) {
@@ -14,8 +15,7 @@ export async function getContactController(req, res, next) {
   const contact = await getContactById(contactId);
 
   if (!contact) {
-    next(new Error('Contacts not found'));
-    return;
+    throw createHttpError(404, 'Contact not found');
   }
 
   res.status(200).json({
