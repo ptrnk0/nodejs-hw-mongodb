dd6148465f9f14becdaf0754914754a4dcf23f6a
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-05 13:01:42 +0200

    Add createContactController for handling new contact creation

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,5 +1,9 @@
 import createHttpError from 'http-errors';
-import { getAllContacts, getContactById } from '../services/contacts.js';
+import {
+  createContact,
+  getAllContacts,
+  getContactById,
+} from '../services/contacts.js';
 
 export async function getAllContactsController(req, res) {
   const contacts = await getAllContacts();
@@ -10,7 +14,7 @@ export async function getAllContactsController(req, res) {
   });
 }
 
-export async function getContactController(req, res, next) {
+export async function getContactController(req, res) {
   const { contactId } = req.params;
   const contact = await getContactById(contactId);
 
@@ -21,6 +25,18 @@ export async function getContactController(req, res, next) {
   res.status(200).json({
     status: 200,
     message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
+}
+
+export async function createContactController(req, res) {
+  console.log(req.body);
+
+  const contact = await createContact(req.body);
+
+  res.status(201).json({
+    status: 201,
+    message: 'Successfully created a contact!',
     data: contact,
   });
 }
