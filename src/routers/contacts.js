d160b8016b6d76ab3fd8b9cf8ed54099c5099b6a
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-05 12:03:58 +0200

    Wrap contact controller routes with error handling utility

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -3,11 +3,11 @@ import {
   getAllContactsController,
   getContactController,
 } from '../controllers/contacts.js';
+import ctrlWrapper from '../utils/ctrlWrapper.js';
 
 const contactsRouter = Router();
 
-contactsRouter.get('/contacts', getAllContactsController);
-
-contactsRouter.get('/contacts/:contactId', getContactController);
+contactsRouter.get('/contacts', ctrlWrapper(getAllContactsController));
+contactsRouter.get('/contacts/:contactId', ctrlWrapper(getContactController));
 
 export default contactsRouter;
