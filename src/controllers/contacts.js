049f1b0253cde8e0f122d558d719237872191f07
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-05 13:44:23 +0200

    Add updateContactController and deleteContactController for managing contacts

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,8 +1,10 @@
 import createHttpError from 'http-errors';
 import {
   createContact,
+  deleteContact,
   getAllContacts,
   getContactById,
+  updateContact,
 } from '../services/contacts.js';
 
 export async function getAllContactsController(req, res) {
@@ -39,4 +41,30 @@ export async function createContactController(req, res) {
     message: 'Successfully created a contact!',
     data: contact,
   });
+}
+
+export async function updateContactContoller(req, res) {
+  const { contactId } = req.params;
+  const contact = await updateContact(contactId, req.body);
+
+  if (!contact) {
+    throw createHttpError(404, 'Contact not found');
+  }
+
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully patched a contact!',
+    data: contact,
+  });
+}
+
+export async function deleteContactController(req, res) {
+  const { contactId } = req.params;
+  const contact = await deleteContact(contactId);
+
+  if (!contact) {
+    throw createHttpError(404, 'Contact not found');
+  }
+
+  res.status(204).send();
 }
